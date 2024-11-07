2431889e90a34444b9a8aa47372f1b33e89c7fe8
HanFaye <3294196260@qq.com>
2024-11-07 23:00:02 +0800

    lab3:正确实现符号表type属性

--- src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
@@ -5,10 +5,15 @@ import javax.lang.model.type.TypeKind;
 import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.lexer.Token;
 import cn.edu.hitsz.compiler.lexer.TokenKind;
+import cn.edu.hitsz.compiler.parser.table.NonTerminal;
 import cn.edu.hitsz.compiler.parser.table.Production;
 import cn.edu.hitsz.compiler.parser.table.Status;
+import cn.edu.hitsz.compiler.parser.table.Term;
+import cn.edu.hitsz.compiler.symtab.SourceCodeType;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.ir.*;
+
+import java.net.IDN;
 import java.util.Stack;
 
 // TODO: 实验三: 实现语义分析
@@ -16,7 +21,8 @@ public class SemanticAnalyzer implements ActionObserver {
 
     public SymbolTable st_in_SA;
 
-    private  Stack<attr> attrstack = new Stack<>();
+    public Stack<Token> tokenStack = new Stack<>();
+
 
     @Override
     public void whenAccept(Status currentStatus) {
@@ -28,111 +34,56 @@ public class SemanticAnalyzer implements ActionObserver {
     @Override
     public void whenShift(Status currentStatus, Token currentToken) {
         // TODO: 该过程在遇到 shift 时要采取的代码动作
-        // 是数字
-        if (currentToken.getKindString().equals("IntConst")) {
-            var new_temp = new attr(true,
-                    Integer.parseInt(currentToken.getTextString()));
-            attrstack.push(new_temp);
-            System.out.println(new_temp);
-        } else {
-            attrstack.push(new attr(false, -1));
-        }
+        tokenStack.push(currentToken);
     }
 
     @Override
     public void whenReduce(Status currentStatus, Production production) {
         // TODO: 该过程在遇到 reduce production 时要采取的代码动作
-        int rightlen = production.body().size();
-        int f_attr_val = -1;
-        System.out.println(production.index());
-        boolean f_attr_computed = false;
-        if (production.index() == 7) {
-            //????
+        // System.out.println(tokenStack);
+
+        Token top;
+        switch (production.index()) {
+            case 5: //D -> int;
+                top = tokenStack.pop(); 
+                var tokenD = Token.normal(top.getKindId(), "nonterminal D");
+                tokenStack.push(tokenD);
+                break;
+            case 4: //S -> D id;
+                var idout = tokenStack.pop();
+                var Dout = tokenStack.pop();
+                Token tokenS = null;
+                tokenStack.push(tokenS);
+                setSymbolType(idout.getTextString(), Dout.getKindString());
+                break;
+            default:
+                break;
         }
-        // 不知道在干嘛
-        // switch (production.index()) {
-        //     case 1:
-        //         break;
-        //     case 2:
-        //         break;
-        //     case 3:
-        //         break;
-        //     case 4:
-        //         break;
-        //     case 5:
-        //         break;
-        //     case 6:
-        //         break;
-        //     case 7:
-        //         f_attr_computed = true;
-        //         f_attr_val = getTopAttr(1);
-        //         break;
-        //     case 8:
-        //         f_attr_computed = true;
-        //         f_attr_val = getTopAttr(3) + getTopAttr(1);
-        //         break;
-        //     case 9:
-        //         f_attr_computed = true;
-        //         f_attr_val = getTopAttr(3) - getTopAttr(1);
-        //     case 10:
-        //         f_attr_computed = true;
-        //         f_attr_val = getTopAttr(1);
-        //         break;
-        //     case 11:
-        //         f_attr_computed = true;
-        //         f_attr_val = getTopAttr(3) * getTopAttr(1);
-        //         break;
-        //     case 12:
-        //         f_attr_computed = true;
-        //         f_attr_val = getTopAttr(1);
-        //         break;
-        //     case 13:
-        //         f_attr_computed = true;
-        //         f_attr_val = getTopAttr(2);
-        //         break;
-        //     case 14:
-        //         f_attr_computed = true;
-        //         f_attr_val = getTopAttr(1);
-        //         break;
-        //     case 15:
-        //         f_attr_computed = true;
-        //         f_attr_val = getTopAttr(1);
-        // }
-        for (int i = 0; i < rightlen; i++) {
-            attrstack.pop();
+    
+    }
+
+    private void setSymbolType(String var_name, String var_type){
+        // System.out.println("set variable type begin");
+        if(st_in_SA.get(var_name).getType() != null){
+            return;
         }
-        attrstack.push(new attr(f_attr_computed, f_attr_val));
+        var symbol_tb_entery = st_in_SA.get(var_name);
+        switch (var_type) {
+            case "int":
+                // System.out.printf("set variable %s to IntConst\n", var_name);
+                symbol_tb_entery.setType(SourceCodeType.Int);
+                break;
+            default:
+                break;
+        }
+        st_in_SA.printSymbolTable();
+        // System.out.println("set variable type end");
     }
 
     @Override
     public void setSymbolTable(SymbolTable table) {
         // TODO: 设计你可能需要的符号表存储结构
         this.st_in_SA = table;
-        System.out.println("SymbolTable in SA:");
-        st_in_SA.printSymbolTable();
     }
 
-    // 从顶部1开始
-    private int getTopAttr(int index) {
-        if(index > attrstack.size() || index < 1) {
-            throw new RuntimeException("Index out of range");
-        }
-        var p = attrstack.get(attrstack.size() - index);
-        if(p.is_computable==false){
-            throw new RuntimeException("cant compute");
-        }else{
-            return p.type;
-        }
-    }
-
-    public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
-        s.push(5);
-        int topindex = 3;//从1开始
-        System.out.println(s.get(s.size() - topindex));
-    }
 }
